ec2a67c2e73bc5dff7a76f64f8d6212cd4731ff4
nathan khosla <nathan@nkhosla.com>
2014-04-28 20:50:21 -0400

    damnit

--- motorTest.py
+++ motorTest.py
@@ -1,7 +1,8 @@
 from motorPWM import MotorDriverHandler
+import time
 
 handler = MotorDriverHandler(17, 18)
 
 while True:
     handler.respondToPIDSuggestedAccel(9)
-    sleep(0.5)
+    time.sleep(0.5)
