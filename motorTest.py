46666077a92ea4e0a22bb4b2ddb1b053519a4780
nathan khosla <nathan@nkhosla.com>
2014-04-28 21:09:04 -0400

    fuck python

--- motorTest.py
+++ motorTest.py
@@ -15,14 +15,14 @@ svo.set_servo(17, 18000.0)
 #while True:
 GPIO.output(18, True)
 
-    """
+"""
     svo.set_servo(17, 18000.0)
     GPIO.output(18, False)
     print "switch"
     time.sleep(0.5)
     svo.set_servo(17, 10000.0)
     GPIO.output(18, True)
-    """
+"""
 
 
     
