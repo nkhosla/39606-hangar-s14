31fe032cdf561fc6c4d07b5a05248ebfd6b94bbe
nathan khosla <nathan@nkhosla.com>
2014-04-28 20:58:24 -0400

    rewrote the test script

--- motorTest.py
+++ motorTest.py
@@ -1,8 +1,21 @@
 from motorPWM import MotorDriverHandler
 import time
+from RPIO import PWM
+import RPi.GPIO as GPIO
 
 handler = MotorDriverHandler(17, 18)
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+svo = PWM.Servo()
+
+svo.set_servo(17, 18000.0 /20000.0)
+
 while True:
-    handler.respondToPIDSuggestedAccel(9)
+    GPIO.output(18, False)
     time.sleep(0.5)
+    GPIO.output(18, True)
+
+
+    
