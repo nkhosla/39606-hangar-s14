7d28eb41b042ca916a180305d52ef0475172f5c5
nathan khosla <nathan@nkhosla.com>
2014-04-28 21:02:19 -0400

    test case, bitches!

--- motorTest.py
+++ motorTest.py
@@ -13,10 +13,16 @@ svo = PWM.Servo()
 svo.set_servo(17, 18000.0)
 
 while True:
+    GPIO.output(18, True)
+
+    """
+    svo.set_servo(17, 18000.0)
     GPIO.output(18, False)
     print "switch"
     time.sleep(0.5)
+    svo.set_servo(17, 10000.0)
     GPIO.output(18, True)
+    """
 
 
     
