b244fae243acbc6ac3d4b6ab340adb4e52a06538
nathan khosla <nathan@nkhosla.com>
2014-04-28 20:14:48 -0400

    the damping script now interfaces with motor driver

--- damping.py
+++ damping.py
@@ -2,6 +2,7 @@
 
 from accel import Accel
 from PID import PID
+from motorPWM import MotorDriverHandler
 
 # Set up the accelorometer
 accelorometer = Accel()
@@ -12,7 +13,12 @@ accelorometer.wakeFromSleep()
 pid = PID(P=1, I=1, D=1, Derivator=0, Integrator=0, Integrator_max=50, Integrator_min=-50)
 
 
+# Set up the motor driver handler
+mdHandler = MotorDriverHandler(17,18)
 
 while True:
     r= accelorometer.getReadingX()
-    print r, pid.update(r)
+    pidOutput = pid.update()
+    mdHandler.respondToPIDSuggestedAccel(pidOutput)
+
+    #print r, pid.update(r)
