330f8f6bafcaf2376dad096c9a0a4916954f7027
nathan khosla <nathan@nkhosla.com>
2014-04-28 20:45:12 -0400

    update

--- motorTest.py
+++ motorTest.py
@@ -3,5 +3,5 @@ from motorPWM import MotorDriverHandler
 handler = MotorDriverHandler(17, 18)
 
 while True:
-    handler.respondToPIDSuggestedAccel(10)
+    handler.respondToPIDSuggestedAccel(9)
     sleep(0.5)
